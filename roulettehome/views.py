69099311280a752bddeef85a405aa7fce14e66dc
unknown <jjew-woo>
2019-07-27 23:17:53 +0900

    a

--- roulettehome/views.py
+++ roulettehome/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 
-from .models import KoreanFood, ChineseFood, JapaneseFood, WesternFood, FastFood, Dessert, Drink
-
 # Create your views here.
 def home(request):
     return render(request,'home.html')
